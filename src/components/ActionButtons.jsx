d57fe9ead26c0cf0e6f273f20f54f7077f5953f7
ThomasLlopiz <toomillopiz@gmail.com>
2024-09-13 11:57:11 -0300

    dividiendo componentes

---
+++ src/components/ActionButtons.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const ActionButtons = ({ onReset, onUndo, onBack }) => {
+  return (
+    <div className="fixed bottom-0 right-0 mt-4 space-x-4 pr-4">
+      <button onClick={onReset} className="bg-red-500 p-2 rounded">
+        Resetear
+      </button>
+      <button onClick={onUndo} className="bg-yellow-500 p-2 rounded">
+        Deshacer
+      </button>
+      <button onClick={onBack} className="bg-blue-500 p-2 rounded">
+        Volver
+      </button>
+    </div>
+  );
+};
