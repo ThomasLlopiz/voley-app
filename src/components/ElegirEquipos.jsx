ae7bff41d45330823198eb763efe117677683e36
ThomasLlopiz <toomillopiz@gmail.com>
2024-09-14 13:30:13 -0300

    estilos

--- src/components/ElegirEquipos.jsx
+++ src/components/ElegirEquipos.jsx
@@ -47,7 +47,7 @@ export const ElegirEquipos = () => {
           onChange={handleSelect1}
           className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
         >
-          <option value="">Seleccione un equipo</option>
+          <option value="">Equipo Uno</option>
           {equipos.map((equipo, index) => (
             <option key={index} value={equipo}>
               {equipo}
@@ -67,7 +67,7 @@ export const ElegirEquipos = () => {
           onChange={handleSelect2}
           className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
         >
-          <option value="">Seleccione un equipo</option>
+          <option value="">Equipo Dos</option>
           {equiposDisponibles2.map((equipo, index) => (
             <option key={index} value={equipo}>
               {equipo}
